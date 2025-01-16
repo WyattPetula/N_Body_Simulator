27a50f9ca2ff4fbd1dccd435a581f294639bc9d5
Wpetula <wyattpeterpetula@gmail.com>
2025-01-16 15:54:38 -0500

    UI Implementation 5
    Fixed incompatibility with VStudio 2022; tinkered with adding panels to scrollview

--- Assets/Spawn_Config.cs
+++ Assets/Spawn_Config.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class Spawn_Config : MonoBehaviour
 {
     public GameObject config_ui_object;
+    private GameObject prev_config_panel;
+    private GameObject new_config;
     public GameObject scrollview_content_object;
 
-    public void SpawnConfigs(){
-        //Instantiate(config_ui_object, scrollview_content_object.transform.position + );
+    private void Awake()
+    {
+        if (prev_config_panel != null)
+        {
+            new_config = Instantiate(config_ui_object, prev_config_panel.transform.position - new Vector3(0, 18, 0), Quaternion.identity, scrollview_content_object.transform);
+        }
+        else
+        {
+            new_config = Instantiate(config_ui_object, scrollview_content_object.transform.position, Quaternion.identity);
+        }
+        prev_config_panel = new_config;
     }
 }

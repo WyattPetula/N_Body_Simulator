9189c2f53c7a1ff2247a9347609da360df4b01ce
Wpetula <wyattpeterpetula@gmail.com>
2025-01-16 00:09:47 -0500

    UI Implementation 4
    Added scroll functionality to config spawner; fixed buttons not pressing correctly; began scripting

---
+++ Assets/Spawn_Config.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Config : MonoBehaviour
+{
+    public GameObject config_ui_object;
+    public GameObject scrollview_content_object;
+
+    public void SpawnConfigs(){
+        //Instantiate(config_ui_object, scrollview_content_object.transform.position + );
+    }
+}
